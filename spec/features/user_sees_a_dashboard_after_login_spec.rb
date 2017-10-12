36a1db861c8ee817ab91650b4e03d6102a9a5622
Brandon Stewart <boveus@gmail.com>
2017-10-12 00:02:18 -0600

    fix tests

--- spec/features/user_sees_a_dashboard_after_login_spec.rb
+++ spec/features/user_sees_a_dashboard_after_login_spec.rb
@@ -5,9 +5,10 @@ feature "An authenticated user can view their dashboard" do
   provides and links to playlist creation" do
   visit spotify_login_path
 
-  expect(current_path).to eq(dashboard_index_path)
+  visit top_artists_path
   expect(page).to have_css('#top_artist_1')
   expect(page).to have_css('.top_artist')
+  visit top_tracks_path
   expect(page).to have_css('.top_track')
   expect(page).to have_css('#top_track_7')
   expect(page).to have_link('Create Playlist')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,7 +30,7 @@ RSpec.configure do |config|
     stub_request(:get, "https://api.spotify.com/recommendations?acousticness=36&danceability=73&duration=20&energy=99&instrumentalness=20&key=76&limit=15&liveness=1&loudness=99&mode=34&popularity=8")
     .to_return(body: fixture('recommended_tracks.json'), headers: {content_type: 'application/json; charset=utf-8'})
 
-    stub_request(:get, "https://api.spotify.com/search?q=Michael%20Jackson&type=Artist")
+    stub_request(:get, "https://api.spotify.com/v1/search?q=Michael%20Jackson&type=Artist")
     .to_return(body: fixture('search_results.json'), headers: {content_type: 'application/json; charset=utf-8'})
   end
   config.expect_with :rspec do |expectations|
